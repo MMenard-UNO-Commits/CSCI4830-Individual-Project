f56e888b871097a160df64bd600f371b981391cb
michaelmenard <michaelmenard@localhost>
2020-10-23 15:58:31 -0500

    Inserting assignments working locally and on the server

--- Individual-project-menard/src/TaskInsertMenard.java
+++ Individual-project-menard/src/TaskInsertMenard.java
@@ -1,6 +1,10 @@
 
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -25,17 +29,54 @@ public class TaskInsertMenard extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String assignmentName = request.getParameter("assignmentName");
+        String className = request.getParameter("className");
+        String dateDue = request.getParameter("dateDue");
+        String timeDue = request.getParameter("timeDue");
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+        Connection connection = null;
+        String insertSql = " INSERT INTO MyTableAssignments (id, ASSIGNMENTNAME, CLASSNAME, DATEDUE, TIMEDUE) values (default, ?, ?, ?, ?)";
+
+        try {
+           DBConnectionMenard.getDBConnection();
+           connection = DBConnectionMenard.connection;
+           PreparedStatement preparedStmt = connection.prepareStatement(insertSql);
+           preparedStmt.setString(1, assignmentName);
+           preparedStmt.setString(2, className);
+           preparedStmt.setString(3, dateDue);
+           preparedStmt.setNString(4, timeDue);
+           preparedStmt.execute();
+           connection.close();
+        } catch (Exception e) {
+           e.printStackTrace();
+        }
+
+        // Set response content type
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        String title = "Insert Assignment to the Assignment List";
+        String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
+        out.println(docType + //
+              "<html>\n" + //
+              "<head><title>" + title + "</title></head>\n" + //
+              "<body bgcolor=\"#f0f0f0\">\n" + //
+              "<h2 align=\"center\">" + title + "</h2>\n" + //
+              "<ul>\n" + //
+
+              "  <li><b>Assignment Name</b>: " + assignmentName + "\n" + //
+              "  <li><b>Class Name</b>: " + className + "\n" + //
+              "  <li><b>Time Due</b>: " + dateDue + "\n" + //
+              "  <li><b>Date Due</b>: " + timeDue + "\n" + //
+
+              "</ul>\n");
+
+        out.println("<a href=/Individual-project-menard/TaskListControl.html>Back to Assignment Manager</a> <br>");
+        out.println("</body></html>");
+     }
+
+     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+     }
 
 }
